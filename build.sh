63c6b0c3add0cca94c99cada47f9042ed292eb0b
Gregor Niehl <gregorniehl@web.de>
2025-08-15 20:50:20 +0200

    code: Use minted instead of listings

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-pdflatex abschlussbericht.tex
+
+# According to https://ftp.rrze.uni-erlangen.de/ctan/macros/latex/contrib/minted/minted.pdf,
+# the "-shell-escape" option is no longer required as of TeX Live 2024 and later.
+pdflatex -shell-escape abschlussbericht.tex
 bibtex abschlussbericht.aux
-pdflatex abschlussbericht.tex
-pdflatex abschlussbericht.tex
+pdflatex -shell-escape abschlussbericht.tex
+pdflatex -shell-escape abschlussbericht.tex
